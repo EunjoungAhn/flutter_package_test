ebf574e7dde2b8ad89b6a0d20b4bee7a3957f933
EunjoungAhn <aej9300@gmail.com>
2022-11-21 18:21:13 +0900

    toast 코드 수정

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -9,6 +9,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late FToast fToast;
+
+  // initState 화면이 처음 호출될때 한번만 호출된다.
+  // 화면이 변경을 감지하면 build가 호출된다.
+  @override
+  void initState() {
+    super.initState();
+    fToast = FToast();
+    fToast.init(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +29,8 @@ class _HomePageState extends State<HomePage> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           ElevatedButton(onPressed: () {
+            _showToast();
+            /*
             Fluttertoast.showToast(
                 msg: "안녕!",
                 toastLength: Toast.LENGTH_SHORT,
@@ -27,6 +40,7 @@ class _HomePageState extends State<HomePage> {
                 textColor: Colors.white,
                 fontSize: 16.0
             );
+            */
           },
           child: const Text('btn'),
         ),
@@ -35,4 +49,44 @@ class _HomePageState extends State<HomePage> {
     ),
     );
   }
+
+ void _showToast() {
+    Widget toast = Container(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+        decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(25.0),
+        color: Colors.greenAccent,
+        ),
+        child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: const [
+            Icon(Icons.check),
+            SizedBox(
+            width: 12.0,
+            ),
+            Text("This is a Custom Toast"),
+        ],
+        ),
+    );
+
+
+    fToast.showToast(
+        child: toast,
+        gravity: ToastGravity.BOTTOM,
+        toastDuration: const Duration(seconds: 2),
+    );
+    
+    // Custom Toast Position
+    fToast.showToast(
+        child: toast,
+        toastDuration: const Duration(seconds: 2),
+        positionedToastBuilder: (context, child) {
+          return Positioned(
+            top: 16.0,
+            left: 16.0,
+            child: child,
+          );  
+        });
+    }
+
 }
